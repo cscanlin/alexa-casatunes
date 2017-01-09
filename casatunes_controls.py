347a9b614b12401d5cb89af69b5e97b6dd7ff2d5
Chris Scanlin <cscanlin@gmail.com>
2017-01-08 22:22:42 -0800

    refactor CasaService app

--- casatunes_controls.py
+++ casatunes_controls.py
@@ -1,46 +1,19 @@
-import logging
+from flask import Flask
+from flask_ask import Ask
 
-from flask import Flask, json
-from flask_ask import Ask, statement, request
-
-import requests
+from casa_service import CasaService
 
 app = Flask(__name__)
 ask = Ask(app, '/')
-logger = logging.getLogger('flask_ask')
-logger.setLevel(logging.INFO)
 
-def load_zone_map(zone_map_file_name):
-    with open(zone_map_file_name) as zone_file:
-        return {k.lower(): v for k, v in json.load(zone_file).items()}
-
-class CasaControls(object):
-    CASA_SERVER_IP = 'http://192.168.1.20'
-    CASA_SERVICE_ROUTE = 'CasaTunes/CasaService.svc'
-    HEADERS = {'Content-Type': 'application/json'}
-    ROOM_ZONE_MAP = load_zone_map('speechAssets/Rooms.json')
-
-    @staticmethod
-    def casa_route(endpoint):
-        return '/'.join((
-            CasaControls.CASA_SERVER_IP, CasaControls.CASA_SERVICE_ROUTE, endpoint
-        ))
-
-    @staticmethod
-    def casa_command(endpoint, speech_text, data={"ZoneID": 0}):
-        requests.post(
-            CasaControls.casa_route(endpoint),
-            headers=CasaControls.HEADERS,
-            data=json.dumps(data)
-        )
-        logger.info(speech_text)
-        return statement(speech_text).simple_card(request.type, speech_text)
+class ControlsApp(object):
+    """docstring for ControlsApp."""
 
     @staticmethod
     @ask.intent('AMAZON.ResumeIntent')
     @ask.intent('CasaPlay')
     def play_song():
-        return CasaControls.casa_command(
+        return CasaService.casa_command(
             endpoint='PlaySong',
             speech_text='Playing casa tunes'
         )
@@ -48,7 +21,7 @@ class CasaControls(object):
     @staticmethod
     @ask.intent('AMAZON.PauseIntent')
     def pause_song():
-        return CasaControls.casa_command(
+        return CasaService.casa_command(
             endpoint='PauseSong',
             speech_text='Pausing casa tunes'
         )
@@ -56,7 +29,7 @@ class CasaControls(object):
     @staticmethod
     @ask.intent('AMAZON.PreviousIntent')
     def previous_song():
-        return CasaControls.casa_command(
+        return CasaService.casa_command(
             endpoint='PreviousSong',
             speech_text='Playing previous Song on casa tunes'
         )
@@ -64,7 +37,7 @@ class CasaControls(object):
     @staticmethod
     @ask.intent('AMAZON.NextIntent')
     def next_song():
-        return CasaControls.casa_command(
+        return CasaService.casa_command(
             endpoint='NextSong',
             speech_text='Playing next Song on casa tunes'
         )
@@ -72,38 +45,38 @@ class CasaControls(object):
     @staticmethod
     @ask.intent('CasaTurnRoomOn', mapping={'room': 'Room'}, default={'room': 'kitchen 1'})
     def turn_room_on(room):
-        return CasaControls.casa_command(
+        return CasaService.casa_command(
             endpoint='SetZonePower',
             speech_text='Turning on music in {room}'.format(room=room),
             data={
                 "Power": True,
-                "ZoneID": str(CasaControls.ROOM_ZONE_MAP[room])
+                "ZoneID": str(CasaService.ROOM_ZONE_MAP[room])
             },
         )
 
     @staticmethod
     @ask.intent('CasaTurnRoomOff', mapping={'room': 'Room'})
     def turn_room_off(room):
-        return CasaControls.casa_command(
+        return CasaService.casa_command(
             endpoint='SetZonePower',
             speech_text='Turning off music in {room}'.format(room=room),
             data={
                 "Power": False,
-                "ZoneID": str(CasaControls.ROOM_ZONE_MAP[room])
+                "ZoneID": str(CasaService.ROOM_ZONE_MAP[room])
             },
         )
 
     @staticmethod
     @ask.intent('CasaSetRoomVolume', mapping={'room': 'Room', 'new_volume': 'Volume'})
     def set_room_volume(room, new_volume):
-        return CasaControls.casa_command(
+        return CasaService.casa_command(
             endpoint='SetZoneVolume',
             speech_text='Setting volume in {room} to {new_volume}'.format(
                 room=room, new_volume=new_volume
             ),
             data={
                 "Volume": new_volume,
-                "ZoneID": str(CasaControls.ROOM_ZONE_MAP[room])
+                "ZoneID": str(CasaService.ROOM_ZONE_MAP[room])
             },
         )
 
