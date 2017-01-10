05036a64278c519b44083caa5c546f3b4f85dcd6
Chris Scanlin <cscanlin@gmail.com>
2017-01-09 23:57:45 -0800

    mostly working initial find and play

--- casatunes_controls.py
+++ casatunes_controls.py
@@ -5,7 +5,9 @@ from flask_ask import Ask, statement, request
 
 import requests
 
-from utils import load_casa_config, parse_app_status, parse_music_search_request
+from utils import load_casa_config, parse_app_status, parse_search_request, parse_search_response
+
+from pprint import pprint
 
 logger = logging.getLogger('flask_ask')
 logger.setLevel(logging.INFO)
@@ -14,13 +16,24 @@ app = Flask(__name__)
 ask = Ask(app, '/')
 
 CASA_CONFIG = load_casa_config('casa_config.json')
+DEFAULT_ZONE = str(CASA_CONFIG['ROOM_ZONE_MAP'][CASA_CONFIG['DEFAULT_ROOM'].lower()])
+
+QUEUE_SPOT_MAP = {
+    'ADD_AND_PLAY': 2,
+}
+
+ALEXA_CASA_TYPE_MAP = {
+    'artist': 'Artists',
+    'album': 'Albums',
+    'song': 'Tracks',
+}
 
 def casa_route(endpoint):
     return '/'.join((
         CASA_CONFIG['SERVER_IP'], CASA_CONFIG['SERVICE_ROUTE'], endpoint
     ))
 
-def casa_command(endpoint, data={"ZoneID": 0}):
+def casa_command(endpoint, data={'ZoneID': 0}):
     return requests.post(
         casa_route(endpoint),
         headers=CASA_CONFIG['HEADERS'],
@@ -63,8 +76,8 @@ def turn_room_on(room):
     r = casa_command(
         endpoint='SetZonePower',
         data={
-            "Power": True,
-            "ZoneID": str(CASA_CONFIG['ROOM_ZONE_MAP'][room.lower()]),
+            'Power': True,
+            'ZoneID': str(CASA_CONFIG['ROOM_ZONE_MAP'][room.lower()]),
         },
     )
     speech_text = 'Turning on music in {room}'.format(room=room),
@@ -75,8 +88,8 @@ def turn_room_off(room):
     r = casa_command(
         endpoint='SetZonePower',
         data={
-            "Power": False,
-            "ZoneID": str(CASA_CONFIG['ROOM_ZONE_MAP'][room.lower()]),
+            'Power': False,
+            'ZoneID': str(CASA_CONFIG['ROOM_ZONE_MAP'][room.lower()]),
         },
     )
     speech_text = 'Turning off music in {room}'.format(room=room),
@@ -87,8 +100,8 @@ def set_room_volume(room, new_volume):
     r = casa_command(
         endpoint='SetZoneVolume',
         data={
-            "Volume": new_volume,
-            "ZoneID": str(CASA_CONFIG['ROOM_ZONE_MAP'][room.lower()])
+            'Volume': new_volume,
+            'ZoneID': str(CASA_CONFIG['ROOM_ZONE_MAP'][room.lower()])
         },
     )
     speech_text = 'Setting volume in {room} to {new_volume}'.format(
@@ -108,11 +121,38 @@ def now_playing_info():
 
 # SEARCH
 @ask.intent('AMAZON.SearchAction<object@MusicCreativeWork>')
-def search_for_creative():
-    # r = casa_command(endpoint='GetAppStatus')
-    search_data = parse_music_search_request(request)
-    print(search_data['creative_type'])
-    speech_text = 'playing the {creative_type} {creative_name}'.format(**search_data)
+def find_and_play_song():
+    search_data = parse_search_request(request)
+    requested_object_type = ALEXA_CASA_TYPE_MAP[search_data['creative_type']]
+
+    search_response = casa_command(
+        endpoint='SearchMediaCollectionByZone',
+        data={
+            'ZoneID': DEFAULT_ZONE,
+            'SearchCurrentMusicServiceOnly': True,
+            'Searchtext': search_data['creative_name'],
+        },
+    )
+    parsed_response = parse_search_response(search_response)
+    first_requested_item_id = parsed_response[requested_object_type][0]['ID']
+
+    casa_command(
+        endpoint='PlayMediaCollectionOrItem2',
+        data={
+            'ZoneID': DEFAULT_ZONE,
+            'ItemID': first_requested_item_id,
+            'Filter': None,
+            'AddToQueue': QUEUE_SPOT_MAP['ADD_AND_PLAY'],
+        },
+    )
+
+    speech_text = 'playing'
+    if search_data['creative_type']:
+        speech_text += ' the {creative_type} {creative_name}'.format(**search_data)
+        if search_data['artist']:
+            speech_text += ' by'
+    if search_data['artist']:
+        speech_text += ' {}'.format(search_data['artist'])
     return speech_response(speech_text)
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
+from collections import defaultdict
 import json
 import xml.etree.ElementTree as ET
+
+def select(arg):
+    pass
 
 def load_casa_config(config_file):
     with open(config_file) as cf:
@@ -7,18 +11,27 @@ def load_casa_config(config_file):
         config['ROOM_ZONE_MAP'] = {k.lower(): v for k, v in config['ROOM_ZONE_MAP'].items()}
     return config
 
-def parse_app_status(casa_response):
+def parse_app_status(status_response):
     parsed_status_data = {}
-    response_data = casa_response.json()['d']
-    now_playing_xml = response_data['NowPlayingInfo'][0]['MediaItem']['DisplayXML']
+    status_data = status_response.json()['d']
+    now_playing_xml = status_data['NowPlayingInfo'][0]['MediaItem']['DisplayXML']
     now_playing_root = ET.fromstring(now_playing_xml)
     for elem in now_playing_root.findall('line'):
         parsed_status_data[elem[0].tag] = elem[0].text
     return parsed_status_data
 
-def parse_music_search_request(request):
-    parsed_search_data = {}
-    slot_data = request['intent']['slots']
-    parsed_search_data['creative_name'] = slot_data['object.name']['value']
-    parsed_search_data['creative_type'] = slot_data['object.type']['value']
-    return parsed_search_data
+def parse_search_request(search_request):
+    parsed_request_data = {}
+    slot_data = search_request['intent']['slots']
+    parsed_request_data['creative_name'] = slot_data.get('object.name', {}).get('value')
+    parsed_request_data['creative_type'] = slot_data.get('object.type', {}).get('value')
+    parsed_request_data['artist'] = slot_data.get('object.byArtist.name', {}).get('value')
+    return parsed_request_data
+
+def parse_search_response(search_response):
+    parsed_response_data = defaultdict(list)
+    search_data = search_response.json()['d']['MediaItems']
+    for item in search_data[1:]:
+        item_creative_type = next(a for a in item['Attributes'] if a['Key'] == 'GroupName')['Value']
+        parsed_response_data[item_creative_type].append(item)
+    return parsed_response_data
