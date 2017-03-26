3d51ea6b7910262b35f51ed5d861ec679a6f13c3
Chris Scanlin <cscanlin@gmail.com>
2017-03-25 17:20:01 -0700

    add quick option to deploy_zappa.sh, remove old virtualenv instructions

--- README.md
+++ README.md
@@ -2,14 +2,6 @@
 
 Control CasaTunes with Alexa
 
-### virtualenv
-
-    /usr/local/bin/python /Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/virtualenv.py -p /usr/local/bin/python alexa-casa
-
-    source alexa-casa/bin/activate
-
-    pip install -r requirements.txt
-
 ### Deploying
 
     docker build -t zappa .
--- deploy_zappa.sh
+++ deploy_zappa.sh
@@ -5,19 +5,26 @@
 
 set -ex
 
+if [[ $1 == -f ]]; then
+  remove_env="rm -rf docker_env &&"
+else
+  remove_env=""
+fi
+
 main () {
+  echo $remove_env
   docker run -ti \
     -e AWS_SECRET_ACCESS_KEY \
     -e AWS_ACCESS_KEY_ID \
     -e AWS_DEFAULT_REGION \
     -v $(pwd):/var/alexa-casatunes --rm zappa \
     bash -c " \
+      $remove_env \
       virtualenv docker_env && \
       source docker_env/bin/activate && \
       cd /var/alexa-casatunes/ && \
       pip install -r requirements.txt && \
-      zappa update dev && \
-      rm -rf docker_env \
+      zappa update dev
     "
 }
 
